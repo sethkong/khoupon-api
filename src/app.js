8debdd1abcdbce11e7d2d067da3bd242920fe0f1
Seth Kong <seth.kong@thehartford.com>
2019-01-29 13:25:47 -0500

    Added simple auth.

--- src/app.js
+++ src/app.js
@@ -16,6 +16,8 @@ const appHooks = require('./app.hooks');
 const channels = require('./channels');
 const mongoose = require('./mongoose');
 
+const authentication = require('./authentication');
+
 const app = express(feathers());
 
 // Load app configuration
@@ -37,6 +39,7 @@ app.configure(mongoose);
 
 // Configure other middleware (see `middleware/index.js`)
 app.configure(middleware);
+app.configure(authentication);
 // Set up our services (see `services/index.js`)
 app.configure(services);
 // Set up event channels (see channels.js)
